4a8f225f089051424ed826fa289bb616e890c21d
Christof Stanits <christofstanits@gmail.com>
2020-06-03 18:42:06 +0200

    Create Entity/Add Components/Mesh and Material

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Mesh unitMesh;
+    [SerializeField] private Material unitMaterial;
+    void Start()
+    {
+        MakeEntity();
+    }
+
+    private void MakeEntity()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        EntityArchetype archetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            );
+        Entity myEntity = entityManager.CreateEntity(archetype);
+        entityManager.AddComponentData(myEntity,new Translation { Value = new float3(2f, 0f, 4f)}); //Unity.Transforms for Translation, Mathematics for float3
+
+        entityManager.AddSharedComponentData(myEntity, new RenderMesh
+        {
+            mesh = unitMesh,
+            material = unitMaterial
+        }); 
+    }
+
+}
